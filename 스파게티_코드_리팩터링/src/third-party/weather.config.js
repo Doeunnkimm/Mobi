79934d0eb05a1a637728198c55f07360518320a4
Doeunnkimm <doeunnkimm@gmail.com>
2023-08-01 18:52:30 +0900

    :spaghetti: 스파게티_코드_리팩터링 : env 파일 인식할 수 있도록 + env로 api key import

--- 스파게티_코드_리팩터링/src/third-party/weather.config.js
+++ 스파게티_코드_리팩터링/src/third-party/weather.config.js
@@ -1,7 +1,6 @@
 export const weatherConfig = (() => {
 	return {
 		api: 'https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0',
-		secret_key:
-			'ntyAlZzGXnI5/YZ0RBYXnqQN/QJUg1rH3D246olgW3+sJqQyJLqJMfA7otuD306nvzEhnuU99duVaYN0YFNviA==',
+		secret_key: import.meta.env.VITE_APP_WEATHER_API_KEY,
 	}
 })()
